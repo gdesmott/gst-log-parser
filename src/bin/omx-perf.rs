c04f4c6fe9e93232e938795edb3d4d87bce8c85a
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2017-12-01 15:48:51 +0100

    omx-perf: count buffer calls/events

--- src/bin/omx-perf.rs
+++ src/bin/omx-perf.rs
@@ -2,6 +2,7 @@
 use std::fs::File;
 use std::io::Write;
 use std::process::exit;
+use std::collections::HashMap;
 
 extern crate gst_log_parser;
 use gst_log_parser::parse;
@@ -20,6 +21,23 @@ struct Opt {
     output: String,
 }
 
+struct Count {
+    empty_call: u32,
+    empty_done: u32,
+    fill_call: u32,
+    fill_done: u32,
+}
+
+impl Count {
+    fn new() -> Count {
+        Count {
+            fill_call: 0,
+            fill_done: 0,
+            empty_call: 0,
+            empty_done: 0,
+        }
+    }
+}
 
 fn generate() -> Result<bool, std::io::Error> {
     let opt = Opt::from_args();
@@ -27,6 +45,7 @@ fn generate() -> Result<bool, std::io::Error> {
     let mut output = (File::create(&opt.output))?;
 
     let parsed = parse(input).filter(|entry| entry.category == "OMX_PERFORMANCE");
+    let mut counts: HashMap<String, Count> = HashMap::new();
 
     for entry in parsed {
         let object = entry.object.unwrap();
@@ -36,7 +55,31 @@ fn generate() -> Result<bool, std::io::Error> {
             let ts = entry.ts.nanoseconds().expect("missing ts");
             write!(output, "{}_{} 1 {}\n", comp_name, entry.message, ts)?;
             write!(output, "{}_{} 0 {}\n", comp_name, entry.message, ts + 1)?;
+
+            let count = counts.entry(comp_name.to_string()).or_insert(Count::new());
+
+            match entry.message.as_ref() {
+                "EmptyThisBuffer" => count.empty_call += 1,
+                "EmptyBufferDone" => count.empty_done += 1,
+                "FillThisBuffer" => count.fill_call += 1,
+                "FillBufferDone" => count.fill_done += 1,
+                _ => (),
+            }
         }
+    }
+
+    for (comp, count) in &counts {
+        println!("{}:", comp);
+        println!(
+            "\tInput (EmptyBufferDone/EmptyThisBuffer): {}/{}",
+            count.empty_done,
+            count.empty_call
+        );
+        println!(
+            "\tOutput (FillBufferDone/FillThisBuffer):  {}/{}",
+            count.fill_done,
+            count.fill_call
+        );
     }
 
     println!("Generated {}", opt.output);
