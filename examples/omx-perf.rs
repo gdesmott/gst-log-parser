b85c5a32bc51f82bf983092a0eee5a0153a3ac77
Guillaume Desmottes <guillaume.desmottes@collabora.com>
2019-06-18 13:43:42 +0530

    add license header to source files

--- examples/omx-perf.rs
+++ examples/omx-perf.rs
@@ -1,3 +1,11 @@
+// Copyright (C) 2017-2019 Guillaume Desmottes <guillaume@desmottes.be>
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
 // Generate input logs with: GST_DEBUG="OMX_API_TRACE:8"
 use std::collections::HashMap;
 use std::fs::File;
